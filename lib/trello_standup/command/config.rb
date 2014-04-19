1e871128eaa0f5f09ae12510d82f774df4cf1275
Alex Li <alex.zige@gmail.com>
2014-04-19 13:12:01 +1200

    Init config for later

---
+++ lib/trello_standup/command/config.rb
@@ -0,0 +1,6 @@
+require "trello_standup/command/base"
+# config trello project related setting
+class TrelloStandup::Command::Config < TrelloStandup::Command::Base
+  def initialize(args)
+  end
+end

58d2fa8997ffaa22f6d4ba0a82a42f4b3fa73920
Alex Li <alex.zige@gmail.com>
2014-04-19 13:14:06 +1200

    Add main Authentication logic
    - login, enter username, open browse with oauth, copy and paste token,
    save into credentials file path.
    - logout remove credentials, force clear cache.

---
+++ lib/trello_standup/auth.rb
@@ -0,0 +1,72 @@
+require "trello_standup"
+require "trello_standup/helpers"
+require "cgi"
+require 'FileUtils'
+
+class TrelloStandup::Auth
+  class << self
+  include TrelloStandup::Helpers
+
+    attr_accessor :credentials
+
+    def username    # :nodoc:
+      get_credentials[0]
+    end
+
+    def token    # :nodoc:
+      get_credentials[1]
+    end
+
+    def get_credentials    # :nodoc:
+      @credentials ||= (read_credentials || ask_for_and_save_credentials)
+    end
+
+    def ask_for_and_save_credentials
+      print "*Enter your trello username:"
+      username = ask
+      sleep 1
+      launchy("Openning Trello authorization page, please confirm access...", trello_oauth_url)
+      print "*After authorization, please copy and paste your token for your trello account:"
+      token = ask
+      self.credentials = [username, token]
+      write_credentials
+      self.credentials
+    end
+
+    def open_trello_oauth_url
+      %x(open #{trello_oauth_url})
+    end
+
+    def credentials_path
+      "#{home_directory}/.trello_standup/credentials"
+    end
+
+    def write_credentials
+      FileUtils.mkdir_p(File.dirname(credentials_path))
+      File.open(credentials_path, 'w') {|credentials| credentials.puts(self.credentials)}
+      FileUtils.chmod(0600, credentials_path)
+    end
+
+    def read_credentials
+      if File.exists?(credentials_path)
+        @credentials = File.read(credentials_path).split("\n")
+      end
+    end
+
+
+    def delete_credentials
+      if File.exists?(credentials_path)
+        FileUtils.rm_f(credentials_path)
+        @credentials = nil
+      end
+    end
+
+    def login
+      get_credentials
+    end
+
+    def logout
+      delete_credentials
+    end
+  end
+end

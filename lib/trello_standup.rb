ce4f6b8150d00b74837b899270ac27affb9c98e4
Alex Li <alex.zige@gmail.com>
2014-04-19 13:03:05 +1200

    Add Global Trello API app_key

--- lib/trello_standup.rb
+++ lib/trello_standup.rb
@@ -1,4 +1,7 @@
 require "trello_standup/version"
+require "trello_standup/cli"
 
 module TrelloStandup
+  APP_KEY = "bb958efe971bcf0f5d0fa920c9b377b3"
+  TRELLO_DOMAIN = "https://trello.com/1/"
 end

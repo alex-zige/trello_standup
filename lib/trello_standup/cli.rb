edc0769f84469abe735d49de5d55fd8744d22045
Alex Li <alex.zige@gmail.com>
2014-04-21 11:58:10 +1200

    Set main entry point for CLI interface

--- lib/trello_standup/cli.rb
+++ lib/trello_standup/cli.rb
@@ -1,5 +1,9 @@
 require "trello_standup"
 require "trello"
+require "trello_standup/helpers"
+require "cgi"
+require "trello_standup/command"
+require "trello_standup/auth"
 
 Trello.configure do |config|
   config.developer_public_key = "bb958efe971bcf0f5d0fa920c9b377b3"
@@ -7,16 +11,43 @@ Trello.configure do |config|
 end
 
 class TrelloStandup::CLI
-  def self.start
-    grouped_actions = Trello::Member.find("alexzige").actions.select { |n| n.type == 'updateCard' && !n.data['listAfter'].nil?}.group_by{ |a| a.data['board']['name']}
-    p "Yesterday"
-    grouped_actions.keys.each do |key|
-      p "Project: *#{key}*"
-      grouped_actions[key].each do |action|
-          if action.data['listAfter']['name'] == "Done" or  action.data['listAfter']['name'] == "Ready for review"
-            p "- #{action.data['card']['name']}"
-          end
-      end
+  class << self
+    include TrelloStandup::Helpers
+  end
+
+  def self.start(*args)
+   begin
+    if $stdin.isatty
+      $stdin.sync = true
     end
+    if $stdout.isatty
+      $stdout.sync = true
+    end
+    command = args.shift.strip rescue "help"
+    TrelloStandup::Command.load
+    TrelloStandup::Command.run(command, args)
+  rescue Interrupt
+    `stty icanon echo`
+    error("Command cancelled.")
+  rescue => error
+    styled_error(error)
+    exit(1)
+  end
+
+end
+
+  #helpers
+  def self.card_is_completed(list_after_name)
+    list_after_name == "Done" ||list_after_name == "Ready for review"
+  end
+
+  def self.is_in_valid_date_range(date)
+    (1.day.ago.beginning_of_day <= date) && ( date <= Time.now.beginning_of_day)
+    # true
+  end
+
+  #open link
+  def open_oauth_link
+    "https://trello.com/1/authorize?key=bb958efe971bcf0f5d0fa920c9b377b3&name=Trello+Standup&expiration=never&response_type=token"
   end
 end
